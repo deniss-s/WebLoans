c4bd4431f8ca84da2f6497144b56fd16b7346c35
elijahkorneckis <ilja.korneckis@gmail.com>
2014-06-22 19:24:53 +0300

    Initial commit

---
+++ src/main/java/lv/iljakorneckis/webloans/Launcher.java
@@ -0,0 +1,16 @@
+package lv.iljakorneckis.webloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by Admin_2 on 2014-06-22.
+ */
+@EnableAutoConfiguration
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+
+}
