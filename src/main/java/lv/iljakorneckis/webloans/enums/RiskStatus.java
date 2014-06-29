c6dbb08c6d73d37cc09a26ee4f39a50b36f432b7
elijahkorneckis <ilja.korneckis@gmail.com>
2014-06-29 16:33:02 +0300

    Adder RiskStatus for going over maximum allowed amount.

--- src/main/java/lv/iljakorneckis/webloans/enums/RiskStatus.java
+++ src/main/java/lv/iljakorneckis/webloans/enums/RiskStatus.java
@@ -3,5 +3,6 @@ package lv.iljakorneckis.webloans.enums;
 public enum RiskStatus {
     OK,
     APPLIED_AFTER_MIDNIGHT,
-    TOO_MANY_APPLICATIONS
+    TOO_MANY_APPLICATIONS,
+    MAX_AMOUNT_EXCEEDED
 }
