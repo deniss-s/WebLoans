3d3124303d6593446750cbea8e39af72fe35f5ec
elijahkorneckis <ilja.korneckis@gmail.com>
2014-06-29 20:31:56 +0300

    Removed unused imports

--- src/test/java/lv/iljakorneckis/webloans/acceptance/RunCukeTests.java
+++ src/test/java/lv/iljakorneckis/webloans/acceptance/RunCukeTests.java
@@ -1,14 +1,7 @@
 package lv.iljakorneckis.webloans.acceptance;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.junit.Cucumber;
-import lv.iljakorneckis.webloans.Launcher;
-import org.apache.catalina.core.ApplicationContext;
 import org.junit.runner.RunWith;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @RunWith(Cucumber.class)
 public class RunCukeTests {
